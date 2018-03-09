062406f0e2a4145918a1ec5be48922698ac3d7c8
liushouchuan13 <liushouchuan13@gmail.com>
2018-03-09 16:17:55 +0800

    models

--- typeidea/comment/models.py
+++ typeidea/comment/models.py
@@ -3,18 +3,17 @@ from __future__ import unicode_literals
 
 from django.db import models
 from django.contrib.auth.models
-
+from blog.models import Post
 # Create your models here.
 
 
 class Comment(models.MODEL):
-    title = models.ForeignKey()
-    username = models.CharField(max_length=50, blank=True, verbose_name='用户名')
+    post = models.ForeignKey(Post, verbose_name='文章')
+    nickname = models.CharField(max_length=50, verbose_name='昵称')
     email = models.EmailField(max_length=254, verbose_name='邮箱')
-    url = models.URLField(verbose_name='网站地址')
-    content　= models.TextField(max_length=300, verbose_name='评论内容')
+    website = models.URLField(verbose_name='网站地址')
+    content　= models.CharField(max_length=2000, verbose_name='评论内容')
     created_time = models.DateTimeField(auto_now_add=True, verbose_name='创建时间')
-    user = models.ForeignKey(User, verbose_name='作者')
 
     class Meta:
         verbose_name = verbose_name_plual = '评论'
